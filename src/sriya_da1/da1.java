8ce26419db37d0f0795e5cf763f42818355c4fd3
matlab <matlab@sjt418scope043>
2026-01-12 16:40:02 +0530

    second commit

--- src/sriya_da1/da1.java
+++ src/sriya_da1/da1.java
@@ -1,5 +1,9 @@
 package sriya_da1;
 
 public class da1 {
-
+public static void main(String[] args) {
+	System.out.println("Student Name: A.Sriya Parigna");
+	System.out.println("Reg No:23MIC0091");
+	System.out.println("Jenkins-Github INtegration Successful");
+}
 }
